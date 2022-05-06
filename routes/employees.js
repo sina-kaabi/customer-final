7e61baacafa9eb054d0e2b6f8606a7e6809e72f2
“Sina <johndoe@example.com>
2022-05-06 01:55:16 +0100

    sorting get requests

--- routes/employees.js
+++ routes/employees.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', () => {
-    console.log('Get request');
+router.get('/', (req,res) => {
+   res.send('Get Request')
 })
 
 
